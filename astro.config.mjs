5e280828e220509f0f9d1cbe067975029310f359
Shiawase <73775954+Shiawaseu@users.noreply.github.com>
2024-10-30 11:36:13 +0300

    Inital commit

---
+++ astro.config.mjs
@@ -0,0 +1,33 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import mdx from '@astrojs/mdx';
+
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+
+  integrations: [
+    tailwind({applyBaseStyles: false}),
+    react(),
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig:
+      {
+        themes: {
+          light: "github-light",
+          dark: "github-dark-default"
+        },
+        wrap: false,
+      },
+    })
+  ],
+
+  adapter: vercel() // Change this if you're planning to deploy somewhere else.
+});
