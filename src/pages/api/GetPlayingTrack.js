d43f09ec9b309f9622c816ca265dbc5e639a4f9e
Shiawase <73775954+Shiawaseu@users.noreply.github.com>
2024-11-16 20:24:58 +0300

    Spotify tracking optimized - Changed how cover URLs are extracted (It's 100% my fault I didn't even bother looking for it in the first place) - You can now click the track name/artist to redirect you to the said destination

--- src/pages/api/GetPlayingTrack.js
+++ src/pages/api/GetPlayingTrack.js
@@ -1,52 +1,4 @@
 import { getCurrentlyPlayingTrack } from '../../utils/spotify';
-import fetch from "node-fetch"
-import * as cheerio from "cheerio";
-
-
-async function fetchSpotifyData(spotifyUrl) {
-  if (!spotifyUrl) return null;
-  try {
-    const embedUrl = `https://open.spotify.com/embed${new URL(spotifyUrl).pathname}`;
-    const embedResponse = await fetch(embedUrl);
-    const embedBody = await embedResponse.text()
-    const $ = cheerio.load(embedBody);
-
-    const pageProps = $('#__NEXT_DATA__').html();
-    if (!pageProps) {
-      throw new Error('Failed to find pageProps!');
-    }
-
-    const pageJson = JSON.parse(pageProps);
-
-    // New update to the embed page
-
-    // For now resolutions are useless, but I'll keep the functionality if Spotify ever provides different resolutions.
-    const entity = pageJson.props.pageProps.state.data.entity;
-    const coverArt = entity.visualIdentity.image;
-
-    let coverArtUrl = '';
-    for (const art of coverArt) {
-      if (art.maxWidth === 300 && art.maxHeight === 300) {
-        coverArtUrl = art.url;
-        break;
-      }
-    }
-
-    if (!coverArtUrl && coverArt.length > 0) {
-      coverArtUrl = coverArt[0].url;
-    }
-
-    if (!coverArtUrl) {
-      throw new Error('Failed to find art url');
-    }
-
-    return coverArtUrl;
-  } catch (error) {
-    throw new Error(`Failed to fetch embed page ${error.message}`);
-  }
-}
-
-
 
 export async function GET() {
     const featureEnabled = import.meta.env["SPOTIFY_ENABLED"];
@@ -76,20 +28,27 @@ export async function GET() {
         headers: { 'Content-Type': 'application/json' },
       });
     }
-    const artistName = trackData.item.artists[0].name;
-    const trackName = trackData.item.name;
-    const trackURL = trackData.item.external_urls.spotify;
-    const startedTime = new Date(trackData.timestamp).toLocaleString();
+    const trackItem = trackData.item
+    const artist = trackItem.artists[0] // Main artist only
+
+    const artistName = artist.name;
+    const trackName = trackItem.name;
+
+    const trackURL = trackItem.external_urls.spotify || null;
+    const artistURL = artist.external_urls.href || null;
+    const trackCover = (trackItem.album.images[0] ? trackItem.album.images[0].url : null);
+    
+    // const startedTime = new Date(trackData.timestamp).toLocaleString();
     const songProgress = trackData.progress_ms
 
-    const trackCover = await fetchSpotifyData(trackURL);
     const result = {
       refreshedToken: FetchResponse.refreshedToken,
       artistName,
+      artistURL,
       trackName,
+      trackURL,
       trackCover,
-      startedTime,
-      songProgress
+      songProgress, // I will hopefully add some functionality for this
     };
     return new Response(JSON.stringify(result), {
       status: 200,
