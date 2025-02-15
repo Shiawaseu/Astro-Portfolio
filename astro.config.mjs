6d8e581e599b29ccdba3b092d45b18b40ae7b878
Shiawase <73775954+Shiawaseu@users.noreply.github.com>
2025-02-15 14:22:22 +0300

    Polish blog pages | Write Part 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,30 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
-
 import vercel from '@astrojs/vercel/serverless';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
 
   integrations: [
-    tailwind({applyBaseStyles: false}),
+    tailwind({ applyBaseStyles: false }),
     react(),
     mdx({
       syntaxHighlight: 'shiki',
-      shikiConfig:
-      {
+      shikiConfig: {
         themes: {
           light: "github-light",
           dark: "github-dark-default"
         },
         wrap: false,
       },
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex]
     })
   ],
 
