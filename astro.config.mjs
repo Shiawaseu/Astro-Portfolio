bbbf5ffe19f84a5e9c036cba734e6f4094b31e60
Shiawaseu <saud876360@gmail.com>
2025-03-20 22:49:34 +0300

    Update blogs to follow content collections (due to nearing deprecation)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex'
 
